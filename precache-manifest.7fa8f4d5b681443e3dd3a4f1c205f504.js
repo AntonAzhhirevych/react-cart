9403f3393e313171d8376e91002e7329767f458b
Anton Azhhirevych <azhhirevych943@gmail.com>
2020-10-11 22:45:24 +0300

    Updates

---
+++ precache-manifest.7fa8f4d5b681443e3dd3a4f1c205f504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a89de9b6591ca7a482b9083559dba97",
+    "url": "/react-cart-test/index.html"
+  },
+  {
+    "revision": "8be1ace74b67c5ccb6e7",
+    "url": "/react-cart-test/static/css/2.5c0455c5.chunk.css"
+  },
+  {
+    "revision": "a1bdc81a50513662bf76",
+    "url": "/react-cart-test/static/css/main.39be54f6.chunk.css"
+  },
+  {
+    "revision": "8be1ace74b67c5ccb6e7",
+    "url": "/react-cart-test/static/js/2.d4e7fbef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cart-test/static/js/2.d4e7fbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bdc81a50513662bf76",
+    "url": "/react-cart-test/static/js/main.814dd998.chunk.js"
+  },
+  {
+    "revision": "32290b559f727c52ca0e",
+    "url": "/react-cart-test/static/js/runtime-main.2200ef87.js"
+  }
+]);
